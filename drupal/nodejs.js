ee1557d24e465edb7e6e41208d240a01f84591c7
Damien Tournoud <damien@commerceguys.com>
2010-06-08 02:01:54 +0200

    Massive clean-up and commenting of nodejs.js.

--- drupal/nodejs.js
+++ drupal/nodejs.js
@@ -1,78 +1,97 @@
 var client = new Faye.Client('http://nodejs.dev.c--g.net:3000/comet', { timeout: 60 });
 
 Drupal.behaviors.nodejs = function() {
-  
   var nid = Drupal.settings.nodejs.nid;
   var process_message = function(message) {
     var tree = message[Drupal.settings.nodejs.page];
     var previous_item = false;
-    for (i in tree) {
+
+    // First, traverse the incoming tree to find new items that need to be
+    // inserted into the DOM.
+    for (i in tree) {
       var comment = tree[i];
-      if ($('#comments a#comment-' + comment.cid).length == 0) {
-        var response = $.ajax({
-          type: "GET",
-          url: Drupal.settings.basePath + '?q=node/' + nid + '/comment/' + comment.cid + '/js',
-          async: false,
-          dataType: 'json'
-        }).responseText;
-        response = Drupal.parseJson(response);
-        var addedComment = $('<div></div>').html(response.data);
-        if (i == 0) {
-          $('div#comments > h2.comments').after(addedComment);
+      if ($('#comments a#comment-' + comment.cid).length > 0) {
+        // The element already exists, skip it.
+        continue;
+      }
+
+      // Fetch the full element via an AJAX request.
+      var response = $.ajax({
+        type: "GET",
+        url: Drupal.settings.basePath + '?q=node/' + nid + '/comment/' + comment.cid + '/js',
+        async: false,
+        dataType: 'json'
+      }).responseText;
+      response = Drupal.parseJson(response);
+
+      // Not sure it is completely necessary to wrap the comment inside
+      // a <div>, but this is consistent with how ahah.js does it.
+      var addedComment = $('<div></div>').html(response.data);
+
+      if (i == 0) {
+        // This is the first element, add it in the begining of the container.
+        $('div#comments > h2.comments').after(addedComment);
+      }
+      else {
+        // The <div> element of the previous comment.
+        var previous_comment = $('#comments a#comment-' + tree[i-1].cid).next("div.comment");
+        // The depth of the previous comment.
+        var previous_depth = tree[i-1].depth;
+
+        if (comment.depth > previous_depth) {
+          // This comment is deeper then the previous one. We need to add
+          // a indented container here if it doesn't exists yet.
+          var wrapper = previous_comment.next(".indented");
+          if (wrapper.length == 0) {
+            wrapper = $('<div class="indented"></div>');
+            previous_comment.after(wrapper);
+          }
+          // Insert this comment at the begining of the new container.
+          wrapper.prepend(addedComment);
         }
         else {
-          var previous_comment = $('#comments a#comment-' + tree[i-1].cid).next("div.comment");
-          var parent_depth = tree[i-1].depth;
-          if (comment.depth > parent_depth) {
-            var wrapper;
-            if(previous_comment.next(".indented").length > 0) {
-              wrapper = previous_comment.next(".indented");
+          // This comment is less deep then the previous one, we need to close
+          // indented containers and start over.
+          if (comment.depth < previous_depth) {
+            for (var i = 0; i < previous_depth - comment.depth; i++) {
+              previous_comment = previous_comment.parent();
             }
-            else {
-              wrapper = $('<div class="indented"></div>');
-              previous_comment.after(wrapper);
-            }
-            wrapper.prepend(addedComment);
           }
-          else {
-            if (comment.depth < parent_depth) {
-              for (var i = 0; i < parent_depth - comment.depth; i++) {
-                previous_comment = previous_comment.parent();
-              }
-            }
-            previous_comment.after(addedComment);
-          }
+          // Insert this comment after the previous one in this container.
+          previous_comment.after(addedComment);
         }
-        addedComment.find('div.comment').hide().fadeIn(500, function() {
-          $(this).animate({ backgroundColor: '#ffffe0' }, 3000, function() {
-            $(this).animate({ backgroundColor: '#ffffff' }, 30000);
-          });
-        });
       }
-    } 
+
+      // Animate the insertion of the comment. This should be made pluggeable.
+      addedComment.find('div.comment').hide().fadeIn(500, function() {
+        $(this).animate({ backgroundColor: '#ffffe0' }, 3000, function() {
+          $(this).animate({ backgroundColor: '#ffffff' }, 30000);
+        });
+      });
+    }
+
+    // Finally, remove from the DOM the comments that are no longer on this page.
+
+    // For performance, prepare a hash of the visible comments.
+    var visible_comments = {}
+    for (i in tree) {
+      comments[tree[i].cid] = true;
+    }
+
+    // Iterate over the DOM and remove the comments that are not in the hash.
     $('#comments a[id^=comment-]').each(function() {
       var cid = $(this).attr('id').substr(8);
-      var found = false;
-      for(i in tree) {
-        if (tree[i].cid == cid) {
-          found = true;
-          break;
+      if (!comments[cid]) {
+        // Remove everything between the <a id="comment-[id]" /> and the div.comment.
+        var current = $('#comments a#comment-' + cid), next;
+        while (current.filter('div.comment').length == 0) {
+          next = current.next();
+          current.remove();
+          current = next;
         }
-      }
-      if (!found) {
-        $('#comments a#comment-' + cid).next("div.comment").remove().end().remove();
       }
     });
   }
-  
-//  process_message([[]]);
+
   client.subscribe('/node/' + Drupal.settings.nodejs.nid, process_message);
-  // var response = $.ajax({
-  //   type: "GET",
-  //   url: Drupal.settings.basePath + '?q=node/' + nid + '/comment/json',
-  //   async: false,
-  //   dataType: 'json'
-  // }).responseText;
-//  response = Drupal.parseJson(response);
-//  process_message(response.data);
 };
